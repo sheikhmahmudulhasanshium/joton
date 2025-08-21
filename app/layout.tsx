3cbba9c653f1d0a7eb3298b21906654668b0a5f2
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shim.aiub@gmail.com>
2025-08-22 00:19:29 +0600

    try-16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 // app/layout.tsx
 
 import type { Metadata } from "next";
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 import "./globals.css";
 
 const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
-
-// This metadata object handles the HTML tag verification method
+const gaId= process.env.NEXT_PUBLIC_GA_ID;
 export const metadata: Metadata = {
   title: {
     default: 'JOTON | Healthcare with hope.',
@@ -36,35 +36,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {/* Google Tag Manager (HEAD) - Using standard <script> tag */}
-        {gtmId && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googagletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${gtmId}');
-              `,
-            }}
-          />
-        )}
+        {/* The component handles adding the GTM script here */}
       </head>
       <body suppressHydrationWarning>
-        {/* Google Tag Manager (BODY) - Using standard <noscript> tag */}
-        {gtmId && (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-              title="GTM"
-            ></iframe>
-          </noscript>
-        )}
         {children}
+        {gtmId && <GoogleTagManager gtmId={gtmId} />}
+        {gaId && <GoogleAnalytics gaId={gaId} />}
+
       </body>
     </html>
   );
