b997b0b53722ca3a0f3d7f39251abf5d1e545769
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shim.aiub@gmail.com>
2025-08-21 23:39:12 +0600

    try-15

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 
 const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
 
+// This metadata object handles the HTML tag verification method
 export const metadata: Metadata = {
   title: {
     default: 'JOTON | Healthcare with hope.',
@@ -43,7 +44,7 @@ export default function RootLayout({
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                'https://www.googagletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','${gtmId}');
               `,
             }}
